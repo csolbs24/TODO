25efd55d4534c4a7c9fb35d10cc36ea93dd9621b
Carter Solberg <csolbs24@gmail.com>
2024-12-13 14:19:01 -0700

    Basic todo without backend

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_options():
     print(f"{TAB}3. Delete Item from List")
     print(f"{TAB}4. Edit Item on List")
     print(f"{TAB}5. Exit Program")
+    print(" ")
     
 
 def get_user_input():
@@ -27,15 +28,46 @@ def get_user_input():
     
     while True:
         if user_input in valid_options:
-            return valid_options
+            return user_input
         user_input = input("That isn't a vaild option. Please try again: ")
+        
+def view_list(todo_list):
+    index = 1
+    for item in todo_list:
+        print(f"{index}: {item}")
+        index += 1
 
+def add_to_list(todo_list):
+    item = input("What item would you like to add to your todo list: ")
+    todo_list.append(item)
+    
+def delete_from_list(todo_list):
+    index = input("What's the index of the element you're deleting: ")
+    todo_list.pop(int(index) - 1)
+
+def edit_list(todo_list):
+    index = input("What's the index of the element you're editing: ")
+    new_element = input("What are you changing it to: ")
+    todo_list[int(index) - 1] = new_element
 
 def main():
     todo_list = []
     while True:
         option = get_user_input()
-        break
+        print(f"option {option}")
+        match (option):
+            case "1":
+                view_list(todo_list)
+            case "2":
+                add_to_list(todo_list)
+            case "3":
+                delete_from_list(todo_list)
+            case "4":
+                edit_list(todo_list)
+            case "5":
+                return
+            case _:
+                print("unknown case")
         
 
 if (__name__ == "__main__"):
