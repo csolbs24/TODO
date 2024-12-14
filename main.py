88177d9afc84059eeaf8c326b0ab66dd62eb1e73
Carter Solberg <csolbs24@gmail.com>
2024-12-14 12:31:59 -0700

    Format with black

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import db
 
-cred = credentials.Certificate('todo-list-cse310-firebase-adminsdk-jmntb-4ad8b50f00.json')
+cred = credentials.Certificate(
+    "todo-list-cse310-firebase-adminsdk-jmntb-4ad8b50f00.json"
+)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()  # Create a Firestore client
 
 # Cloud Firestore
-items_ref = db.collection('items')
+items_ref = db.collection("items")
 
 
 # Tab constant to be used throughout the program
 TAB = " " * 4
 
+
 def write_to_db(todo_list):
     """Write the given todo list to the database"""
     db.document("items/user").set({"items": todo_list})
+
 
 def print_options():
     """Prints a list of options to the command line"""
@@ -29,28 +33,22 @@ def print_options():
     print(f"{TAB}5. Delete Entire List")
     print(f"{TAB}6. Exit Program")
     print(" ")
-    
+
 
 def get_user_input():
     """Ask the user for input until they provid a vaild input."""
-    
-    valid_options = [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6"
-    ]
+
+    valid_options = ["1", "2", "3", "4", "5", "6"]
 
     print_options()
     user_input = input("Type number of selected option: ")
-    
+
     while True:
         if user_input in valid_options:
             return user_input
         user_input = input("That isn't a vaild option. Please try again: ")
-        
+
+
 def view_list(todo_list):
     index = 1
     for item in todo_list:
@@ -58,17 +56,20 @@ def view_list(todo_list):
         index += 1
     print()
 
+
 def add_to_list(todo_list):
     item = input("What item would you like to add to your todo list: ")
     view_list(todo_list)
     todo_list.append(item)
     write_to_db(todo_list)
-    
+
+
 def delete_from_list(todo_list):
     index = input("What's the index of the element you're deleting: ")
     view_list(todo_list)
     todo_list.pop(int(index) - 1)
     write_to_db(todo_list)
+
 
 def edit_list(todo_list):
     index = input("What's the index of the element you're editing: ")
@@ -76,23 +77,25 @@ def edit_list(todo_list):
     view_list(todo_list)
     todo_list[int(index) - 1] = new_element
     write_to_db(todo_list)
-    
+
+
 def delete_everything(todo_list):
     """Make a database request to delete everything in the list"""
     todo_list = []
     write_to_db([])
 
+
 def main():
     # Grab the todo items from the database
     items = items_ref.stream()
-    
+
     # Populate the local todo list with the db items
     todo_list = None
     for item in items:
-        todo_list = item.to_dict()["items"] # The list is stored as the items property
+        todo_list = item.to_dict()["items"]  # The list is stored as the items property
     if todo_list is None:
         todo_list = []
-    
+
     # Continually loop until the user exits
     while True:
         option = get_user_input()
@@ -112,7 +115,7 @@ def main():
                 return
             case _:
                 print("unknown case")
-        
 
-if (__name__ == "__main__"):
+
+if __name__ == "__main__":
     main()
